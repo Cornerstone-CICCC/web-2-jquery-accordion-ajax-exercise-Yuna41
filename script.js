ed6f9a84be7e371c580705035fe326a5e1410328
Yuna41 <uncnprs@gmail.com>
2025-08-08 17:46:09 -0700

    Complete Ex1 and Ex2

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 $(function() {
-  // your code here
+  
+  // Ex1
+  $('.accordion-header').on('click', function(){
+    $('.accordion-header').not(this).next().slideUp()
+    $(this).next().slideToggle()
+  })
+
+  // Ex2
+  $('.todos button').on('click', function(){
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      type: "GET",
+      success: function(response){
+        const todos = response.todos
+        buildTodosList(todos)
+      },
+      error: function(err){
+        console.log(err)
+      }
+    })
+  })
 })
+// .todos ul
+function buildTodosList(arr){
+  const list = $('.todos ul')
+  let itemHtml = ''
+  arr.forEach(el => {
+    itemHtml += `<li>${el.todo}</li>`
+  })
+  list.html(itemHtml)
+}
